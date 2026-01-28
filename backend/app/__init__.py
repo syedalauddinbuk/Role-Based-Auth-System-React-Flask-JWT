ed130d8b558174ec17117159c5eb741bcbea08ec
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-01-27 23:10:52 -0700

    updated __init__

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,3 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 from .extensions import db, jwt
 from config import Config
+
+def create_app():
+    app=Flask(__name__)
+    app.config.from_object(Config)
+    CORS(app)
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
